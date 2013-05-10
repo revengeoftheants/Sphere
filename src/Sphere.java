6f5429a73f6241bfa96be11f30ebc5b11c2770c9
revengeoftheants <kevdean@gmail.com>
2013-05-09 20:09:45 -0500

    Just organizing referenced .jars better.

--- src/Sphere.java
+++ src/Sphere.java
@@ -1,8 +1,3 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 
 import processing.core.*;
